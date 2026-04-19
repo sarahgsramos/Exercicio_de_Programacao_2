0bf6a4f58cf40326c76e797d480e3905f4633cb3
Marilia Frota <mariliafrota11@outlook.com>
2026-04-19 14:42:38 -0300

    Guardar dado

--- funcoes.py
+++ funcoes.py
@@ -8,3 +8,15 @@ def rolar_dados(n):
         i += 1
     return dados
 
+def guardar_dado (lista_dados_rolados, lista_dados_ja_guardados, indice):
+    valor = lista_dados_rolados[indice]
+    nova_lista = []
+    for i in range(len(lista_dados_rolados)):
+        if i != indice:
+            dado = lista_dados_rolados[i]
+            nova_lista.append(dado)
+    lista_dados_ja_guardados.append(valor)
+    saida = [nova_lista, lista_dados_ja_guardados]
+    return saida
+
+
