1380161bf8fd2c0a9e9f0dc44bf1a6b074b174a4
Sarah Ramos <sarahgsramoss2@gmaill.com>
2026-04-19 14:10:50 -0300

    Rolar dados

---
+++ funcoes.py
@@ -0,0 +1,10 @@
+from random import randint
+def rolar_dados(n):
+    dados = []
+    i = 0
+    while i < n:
+        sorteio = randint(1,6)
+        dados.append[sorteio]
+        i += 1
+    return dados
+
